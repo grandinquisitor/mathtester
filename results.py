d37a370dddd6335912f887ef84d284dbe77d37d6
nick <infinityminusnine@gmail.com>
2009-04-09 16:47:05 -0400

    now actually works given log format

--- results.py
+++ results.py
@@ -6,11 +6,15 @@ from pprint import pprint
 fname = 'log.pkl'
 date_ix = 4
 
-filter_by = lambda x: 'mult11' in x[0]
+filter_by = lambda x: 'sub3e' in x[0]
 
 log = pickle.load(open(fname, 'rb'))
 
 for day, g in groupby(ifilter(filter_by, log), key=lambda l: l[date_ix].date()):
     day_result = list(g)
+    print
     print day
-    print ' avg time:', sum(r[2] for r in day_result) / float(len(day_result))
+    print ' avg score:', sum(r[2] for r in day_result) / float(len(day_result))
+    print ' avg time:', sum(r[3] for r in day_result) / float(len(day_result))
+
+print
