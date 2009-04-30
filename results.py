950018ae5393260657bac514a1b88d13d7616375
nick <infinityminusnine@gmail.com>
2009-04-30 10:34:06 -0400

    now shows a complete breakdown of results

--- results.py
+++ results.py
@@ -2,20 +2,35 @@ from itertools import *
 import cPickle as pickle
 import datetime
 from pprint import pprint
+import mathtest
 
 fname = 'log.pkl'
 date_ix = 4
+test_name_ix = 0
 
-filter_by = lambda x: 'm11h' in x[0]
+keyfunc = lambda l: (l[test_name_ix], l[date_ix].date())
 
 log = pickle.load(open(fname, 'rb'))
 
-for day, g in groupby(ifilter(filter_by, log), key=lambda l: l[date_ix].date()):
+last_test = None
+
+for (test, day), g in groupby(sorted(log, key=keyfunc), key=keyfunc):
+
+    if test != last_test:
+        print
+        print
+        try:
+            name = "%s (%s)" % (test, mathtest.possible_tests[test].name)
+        except:
+            name = test
+        print name + "::"
+        last_test = test
+
     day_result = list(g)
     print
-    print day
-    print '         n:', len(day_result)
-    print ' avg score:', sum(r[2] for r in day_result) / float(len(day_result))
-    print '  avg time:', sum(r[3] for r in day_result) / float(len(day_result))
+    print ' ' + str(day)
+    print '          n:', len(day_result)
+    print '  avg score:', sum(r[2] for r in day_result) / float(len(day_result))
+    print '   avg time:', sum(r[3] for r in day_result) / float(len(day_result))
 
 print
