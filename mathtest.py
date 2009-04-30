0782a4b2d2a72aee5cc45caf44f0c09d4f6b8942
nick <infinityminusnine@gmail.com>
2009-04-30 10:56:14 -0400

    lots of rewriting of procedural code, added lots of new tests

--- mathtest.py
+++ mathtest.py
@@ -97,20 +97,22 @@ class m11l(m1):
 
     def setup(self):
         f = random.randint(1, 9)
-        f2 = random.randint(0, 9 - f)
+        f2 = random.randint(1, 10 - f)
         self.x = int(str(f) + str(f2))
         self.y = 11
-
+        self.x, self.y = self.swap(self.x, self.y)
 
 
 class m11h(m1):
     name = "multiply by 11 (high)"
+    hint = "add one to first digit * 100, then add second of sum of both numbers * 10, then second digit"
 
     def setup(self):
         f = random.randint(1, 9)
         f2 = random.randint(10 - f, 9)
         self.x = int(str(f) + str(f2))
         self.y = 11
+        self.x, self.y = self.swap(self.x, self.y)
 
 
 
@@ -120,6 +122,64 @@ class m11b(m1):
     def setup(self):
         self.x = random.randint(10, 99)
         self.y = 11
+        self.x, self.y = self.swap(self.x, self.y)
+
+
+class m21(m1):
+    name = "multiple 2 digit number by a 1 digit number"
+
+    def setup(self):
+        (self.x, self.y) = self.swap(random.randint(10, 99), random.randint(2, 9))
+
+
+class m215(m1):
+    name = "multiple 2 digit number by a 1 digit number (5s)"
+
+    def setup(self):
+        if random.randint(0, 1):
+            (self.x, self.y) = self.swap(
+                50 + random.randint(0, 9),
+                random.randint(1, 4) * 2
+            )
+        else:
+            (self.x, self.y) = self.swap(
+                (random.randint(1, 4) * 20) + random.randint(0, 9),
+                5
+            )
+
+
+class m21e(m1):
+    name = "multiple 2 digit number by a 1 digit number, no carrying"
+
+    def setup(self):
+        self.y = random.randint(2, 9)
+        x1 = random.randint(1,9)
+        x2 = random.randint(0, 9 // self.y)
+        self.x = (x1 * 10) + x2
+        self.x, self.y = self.swap(self.x, self.y)
+
+
+class m219(m1):
+    name = "multiple 2 digit number by 9"
+
+    def setup(self):
+        self.x, self.y = self.swap(9, random.randint(11, 99))
+
+
+class m219nc(m1):
+    name = "multiple 2 digit number by 9, no carrying"
+
+    def setup(self):
+        x2 = random.randint(2, 9)
+        x1 = random.randint(1, x2 - 1)
+        self.x, self.y = self.swap(9, (x1 * 10) + x2)
+
+class m2199(m1):
+    name = "multiple 2 digit number by 99"
+    hint = "4699 = 46x(100-1) = 4600-46 = 4554\n or : first digit, 1 minus second digit then power-of-ten complement"
+
+    def setup(self):
+        self.x, self.y = self.swap(99, random.randint(11, 99))
 
 
 class add3(mathtest):
@@ -183,6 +243,15 @@ class p10c(mathtest):
         return "%s - %s" % (self._commafy(self.x), self._commafy(self.y))
 
 
+class p10cn(p10c):
+    name = "power of 10 complement * n"
+    hint = "all numbers must sum to 9, except the last which must be 10"
+
+    def setup(self):
+        self.x = pow(10, random.randint(2, 6)) * random.randint(2, 9)
+        self.y = random.randint(1, self.x - 1)
+
+
 class sub2b(mathtest):
     name = "2 digit subtraction, any method"
 
@@ -233,6 +302,36 @@ class sub43(sub3):
         self.x = random.randint(1000, 9999)
         self.y = random.randint(100, 999)
 
+class domemod(mathtest):
+    # http://litemind.com/how-to-become-a-human-calendar/
+    name = "domesday modulo"
+
+    def setup(self):
+        self.x = random.randint(0, 6) + random.randint(0, 6) + random.randint(1, 31)
+        self.y = 7
+
+    def getcorrect(self):
+        return self.x % self.y
+
+    def getprompt(self):
+        return "%s %% %s" % (self.x, self.y)
+
+
+class domecoded(mathtest):
+    # http://litemind.com/how-to-become-a-human-calendar/
+    name = "domesday calculations"
+
+    def setup(self):
+        self.yearcode = random.randint(0, 6)
+        self.monthcode = random.randint(0, 6)
+        self.day = random.randint(1, 31)
+
+    def getcorrect(self):
+        return sum((self.yearcode, self.monthcode, self.day)) % 7
+
+    def getprompt(self):
+        return "(%s + %s + %s) mod 7" % (self.yearcode, self.monthcode, self.day)
+
 
 
 somevar = None
@@ -243,13 +342,6 @@ possible_tests = dict(
     for subclass in mathtest.getsubclasses()
     )
 
-
-log_fname = 'log.pkl'
-
-if os.path.exists(log_fname):
-    log = pickle.load(open(log_fname, 'rb'))
-else:
-    log = []
 
 
 
@@ -279,52 +371,66 @@ class datedict(dict):
 
 
 
-n = 0
+if __name__ == '__main__':
 
-try:
-    while True:
+    log_fname = 'log.pkl'
 
-        test_max_date = datedict(log)
+    if os.path.exists(log_fname):
+        log = pickle.load(open(log_fname, 'rb'))
+    else:
+        log = []
 
-        print "which test?"
-        for key, test in sorted(possible_tests.iteritems(), key=lambda x: x[0]):
-            print "%s) %s -- %s" % (key, test.name, test_max_date[key])
-        print "total %s tests taken" % len(log)
 
-        choice = sys.stdin.readline().rstrip().lower()
+    n = 0
 
-        if choice not in possible_tests:
-            print "choice not found"
-            print
+    try:
+        try:
+            while True:
 
-        else:
-            chosen_test = possible_tests[choice]
+                test_max_date = datedict(log)
 
-            if hasattr(chosen_test, 'hint') and chosen_test.hint:
-                print
-                print "hint:", chosen_test.hint
+                print "which test?"
+                for key, test in sorted(possible_tests.iteritems(), key=lambda x: x[0]):
+                    print "%s) %s -- %s" % (key, test.name, test_max_date[key])
+                print "total %s tests taken" % len(log)
 
-            try:
-                for i in itertools.count():
+                choice = sys.stdin.readline().rstrip().lower()
+
+                if choice not in possible_tests:
+                    print "choice not found"
                     print
-                    print "test", i + 1
-                    testobj = chosen_test()
-                    (result, timed) = testobj.runtest()
-                    log.append((testobj.__class__.__name__, testobj.hashme(), result, timed, datetime.now()))
-                    n += 1
-                    time.sleep(1)
 
-            except KeyboardInterrupt:
-                print "exiting this test..."
-                print
-                pass
+                else:
+                    chosen_test = possible_tests[choice]
 
-except KeyboardInterrupt:
-    print "exiting..."
+                    if hasattr(chosen_test, 'hint') and chosen_test.hint:
+                        print
+                        print "hint:", chosen_test.hint
 
-    if n:
-        print "save session? (Y)"
-        answer = sys.stdin.readline().rstrip().upper()
-        if answer in ('', 'Y', 'YE', 'YES'):
-            print "saving..."
-            pickle.dump(log, open(log_fname, 'wb'))
+                    try:
+                        for i in itertools.count():
+                            print
+                            print "test", i + 1
+                            testobj = chosen_test()
+                            (result, timed) = testobj.runtest()
+                            log.append((testobj.__class__.__name__, testobj.hashme(), result, timed, datetime.now()))
+                            n += 1
+                            time.sleep(1)
+
+                    except KeyboardInterrupt:
+                        print "exiting this test..."
+                        print
+                        pass
+
+        except KeyboardInterrupt:
+            pass
+
+
+    finally:
+        print "exiting..."
+        if n:
+            print "save session? (Y)"
+            answer = sys.stdin.readline().rstrip().upper()
+            if answer in ('', 'Y', 'YE', 'YES'):
+                print "saving..."
+                pickle.dump(log, open(log_fname, 'wb'))
