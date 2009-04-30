b88086ecb72a07b21841ba594553562c66487691
nick <infinityminusnine@gmail.com>
2009-04-30 10:16:20 -0400

    shows 'n'

--- results.py
+++ results.py
@@ -6,7 +6,7 @@ from pprint import pprint
 fname = 'log.pkl'
 date_ix = 4
 
-filter_by = lambda x: 'sub3e' in x[0]
+filter_by = lambda x: 'm11h' in x[0]
 
 log = pickle.load(open(fname, 'rb'))
 
@@ -14,7 +14,8 @@ for day, g in groupby(ifilter(filter_by, log), key=lambda l: l[date_ix].date()):
     day_result = list(g)
     print
     print day
+    print '         n:', len(day_result)
     print ' avg score:', sum(r[2] for r in day_result) / float(len(day_result))
-    print ' avg time:', sum(r[3] for r in day_result) / float(len(day_result))
+    print '  avg time:', sum(r[3] for r in day_result) / float(len(day_result))
 
 print
